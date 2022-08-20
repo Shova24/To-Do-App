cf65d5aaffbc84f79188fff7d5ab516104784547
Shova <you@example.com>
2022-08-20 16:40:36 +0600

    Router Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import HomePage from "./Components/HomePage/HomePage";
 import "./index.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Breadcrumb } from "antd";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </>
   );
 }
--- src/Components/Shared/BreadCrumb.jsx
+++ src/Components/Shared/BreadCrumb.jsx
@@ -6,16 +6,15 @@ import HomePage from "../HomePage/HomePage";
 export default function BreadCrumbComp() {
   return (
     <>
-      <BrowserRouter>
-        <Breadcrumb style={{ marginLeft: "13%", paddingBottom: "25px" }}>
-          <Breadcrumb.Item>
-            {/* <Route path="/" element={<HomePage />} /> */}
-            <Link to="/">home</Link>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>Add Task</Breadcrumb.Item>
-          <Breadcrumb.Item>Deleted Task</Breadcrumb.Item>
-        </Breadcrumb>
-      </BrowserRouter>
+      <Breadcrumb style={{ marginLeft: "13%", paddingBottom: "25px" }}>
+        <Breadcrumb.Item>
+          <Link to={"/"}>Home</Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>
+          <Link to={"/add task"}>Add Task</Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>Deleted Task</Breadcrumb.Item>
+      </Breadcrumb>
     </>
   );
 }
